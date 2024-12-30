8ed3644cafd377cd987a78d5a33757ecf19492b7
lazzzykim <33sunghyeon@gmail.com>
2024-12-30 23:41:48 +0900

    컬렉션 - ArrayList

---
+++ src/collection/array/MyArrayListV1.java
@@ -0,0 +1,26 @@
+package collection.array;
+
+public class MyArrayListV1 {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV1() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV1(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(Object e) {
+        elementData[size] = e;
+        size++;
+    }
+}
